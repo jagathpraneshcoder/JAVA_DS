ec7faf1840572dce4376c3ab4ba5fc137362aed8
jagathpraneshrameshbabu@gmail.com <jagathpraneshrameshbabu@gmail.com>
2024-09-20 16:39:07 +0530

    New Changes to shape 1

--- polymorphism/Shapes1/Main.java
+++ polymorphism/Shapes1/Main.java
@@ -1,7 +1,11 @@
 package JAVA_OOPS.Polymorphism.Shapes1;
 
+import jdk.dynalink.support.SimpleRelinkableCallSite;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        Scanner sc = new Scanner();
     }
 }
--- polymorphism/Shapes1/Shapes.java
+++ polymorphism/Shapes1/Shapes.java
@@ -2,6 +2,9 @@ package JAVA_OOPS.Polymorphism.Shapes1;
 
 public class Shapes {
     void getArea(){
-
+        return 0;
+    }
+    void getPerimeter(){
+        return 0;
     }
 }
